7a31268265fb3a64e23ea8a3a902959a703278d1
francelino <fmarcilio1989@gmail.com>
2022-05-08 09:53:44 -0300

    implementando o serviço de salvar contato

--- src/app/contato.service.ts
+++ src/app/contato.service.ts
@@ -1,9 +1,18 @@
+import { Contato } from './contato/contato';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContatoService {
+  url: string = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
+
+  save(contato: Contato): Observable<Contato> {
+    return this.http.post<Contato>(this.url, contato);
+  }
 }
--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,15 +1,29 @@
+import { ContatoService } from './../contato.service';
 import { Component, OnInit } from '@angular/core';
+import { Contato } from './contato';
 
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
-  styleUrls: ['./contato.component.css']
+  styleUrls: ['./contato.component.css'],
 })
 export class ContatoComponent implements OnInit {
-
-  constructor() { }
+  constructor(private contatoService: ContatoService) {}
 
   ngOnInit(): void {
-  }
+    const contato: Contato = new Contato();
+    contato.nome = 'Francelino Silva';
+    contato.email = 'francelino.silva@gmail.com';
+    contato.favorito = false;
 
+    this.contatoService.save(contato).subscribe(
+      (success) => {
+        console.log('gravado com sucesso!');
+      },
+      (error) => {
+        console.log('error', error);
+      },
+      () => console.log('post completo!')
+    );
+  }
 }
